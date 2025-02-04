b042dc354234d69c527ac1511a77f983776aef2d
Hadiyal Vijay <hadiyalvijay7777@gmail.com>
2025-02-04 11:28:52 +0530

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from "react";
 import SellerDashboard from './Dashboard/SellerDashboard';
 
-
 function App() {
+  const [dataUpdated, setDataUpdated] = useState(false);
 
+  // Function to trigger re-render without refreshing
+  const refreshDashboard = () => {
+    setDataUpdated(prev => !prev); // Toggle state to force re-render
+  };
 
   return (
     <>
-   <SellerDashboard />
+      <SellerDashboard refreshDashboard={refreshDashboard} dataUpdated={dataUpdated} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
